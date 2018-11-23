7576a045d606cecc2c71a3f2287e1f54ff38d8d7
Ricaro Guevara <ricardogp@protonmail.com>
2018-11-23 02:00:23 -0500

    trafic motion assets

---
+++ simulation/Assets/trafic_scripts/car_motion.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class car_motion : MonoBehaviour {
+    
+	// Update is called once per frame
+	void Update () {
+        transform.Translate(20f * Time.deltaTime, 0f, 0f);
+    }
+}
