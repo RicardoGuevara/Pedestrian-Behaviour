9cea96b28f67fd3341ee18e81eaef30d1f8bd490
Ricardo Guevara <ricardogp@protonmail.com>
2019-02-23 11:44:39 -0500

    demo con proyeccion, movimientos de camara, spawners

---
+++ comportamiento_peatones/demo/VrPedBehav/Assets/Scripts/car_motion.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class car_motion : MonoBehaviour {
+    
+	// Update is called once per frame
+	void Update () {
+        transform.Translate(20f * Time.deltaTime, 0f, 0f);
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag.Equals("rot_node"))
+        {
+            transform.Rotate(new Vector3(0,90,0), 20f * Time.deltaTime);
+        }
+    }
+}
---
+++ comportamiento_peatones/demo/VrPedBehav/Assets/Scripts/random_interval_generation.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class random_interval_generation : MonoBehaviour {
+
+    public GameObject auto1,    //base_car
+                      auto2,    //base_cam
+                      auto3;    //base_bus
+
+    public Transform origen;    // base pos 
+
+    // Use this for initialization
+    
+    void Update()
+    {
+        StartCoroutine(delay_and_create(gen_random_delay()));
+    }
+
+    private bool isCoroutineExecuting = false;
+
+    IEnumerator delay_and_create(float time)
+    {
+        if (isCoroutineExecuting) yield break;
+
+        isCoroutineExecuting = true;
+
+        yield return new WaitForSeconds(time);
+
+        gen_trafic();
+        //Instantiate(auto1, origen.position, origen.rotation);
+
+        isCoroutineExecuting = false;
+    }
+
+    float gen_random_delay()
+    {
+        return Random.Range(0.5f, 5f);
+    }
+
+    void gen_trafic()
+    {
+        int model = Random.Range(1, 4);
+
+        switch (model)
+        {
+            case 1:
+                Instantiate(auto1, origen.position, origen.rotation);
+                break;
+            case 2:
+                Instantiate(auto2, origen.position, origen.rotation);
+                break;
+            case 3:
+                Instantiate(auto3, origen.position, origen.rotation);
+                break;
+        }
+    }
+}
