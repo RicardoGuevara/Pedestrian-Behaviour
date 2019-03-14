c038e580980dc7a6a174da6fc467a417567de4df
Ricardo Guevara <ricardogp@protonmail.com>
2019-03-14 09:14:13 -0500

    corregido, exportado apk

---
+++ comportamiento_peatones/demo/emergencia/Assets/Raw Mocap Data/peep_motion.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class peep_motion : MonoBehaviour {
+
+	// Update is called once per frame
+	void Update () {
+        transform.Translate(2f * Time.deltaTime, 0f, 0f);
+    }
+}
