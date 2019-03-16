409ac94e15e51b37a97f25fbc919d4ab1a029061
Ricardo Guevara <ricardogp@protonmail.com>
2019-03-16 11:08:18 -0500

    fixing collisions

--- comportamiento_peatones/demo/emergencia/Assets/Scripts/movimiento_continuo.cs
+++ comportamiento_peatones/demo/emergencia/Assets/Scripts/movimiento_continuo.cs
@@ -5,18 +5,19 @@ using UnityEngine;
 public class movimiento_continuo : MonoBehaviour
 {
     public float speed;
-    // Start is called before the first frame update
+
     void Start()
     {
-        
+
     }
-    
-    
+
     void Update()
     {
         transform.Translate(0f,0f,speed * Time.deltaTime);    
+        
     }
 
+   
     private void OnCollisionEnter(Collision col)
     {
         if (col.transform.tag == "rotator_node")
---
+++ comportamiento_peatones/demo/emergencia/Assets/Scripts/trasher.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class trasher : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    void OnCollisionEnter(Collision col)
+    {
+        Debug.Log("ahijue");
+        if (true)
+        { Destroy(col.gameObject); } 
+    }
+        
+        
+        
+}
