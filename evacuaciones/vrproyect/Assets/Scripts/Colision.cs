1ebe91a65d60d7c5f32044b24ba1cd29e2ec1ef5
vrlls <seviloria@uninorte.edu.co>
2019-04-02 18:13:17 -0500

    Actualizacion

---
+++ evacuaciones/vrproyect/Assets/Scripts/Colision.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+public class Colision : MonoBehaviour {
+    private void OnCollisionEnter(Collision collision)
+    {
+        this.transform.position = new Vector3(-381, 0, 0);
+    }
+}
---
+++ evacuaciones/vrproyect/Assets/script prueba.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class scriptprueba : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		//camnios
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
