9cc6ca9aeee8fd0bb4c6a12e2f09b4ed89bd7ef1
RicardoGuevara <ricardogp@protonmail.com>
2019-04-09 19:39:11 -0500

    cambios escenario 1

---
+++ comportamiento_peatones/demo/emergencia/Assets/cinematicintro.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class cinematicintro : MonoBehaviour {
+	
+	void Start () {
+		//this.GetComponent<Animator>().Play("CameraRoll",-1,0.0f);
+
+
+	}
+
+	bool generalsw =true;
+	bool onrotate =false;
+	int rot = 0;
+	
+	// Update is called once per frame
+	void Update () {
+		if (generalsw) {
+			if (!onrotate) {
+				transform.Translate (10f * Time.deltaTime, 0f, 0f);
+			} else {
+				if (rot < 2500f*Time.deltaTime) {
+					gameObject.transform.Rotate (0f, 15f * Time.deltaTime, 0f);
+					rot++;
+				} else {
+					gameObject.transform.SetPositionAndRotation (new Vector3 (-38.8f, 1.9f, -32.5f), new Quaternion (0f, 0f, 0f, 0f));
+					generalsw = false;
+				}
+			}
+		}
+	}
+
+
+	void OnTriggerEnter(Collider col)
+	{
+		//Debug.Log("contacto detectado");
+		/*
+		if (col.gameObject.CompareTag("stop"))
+		{ 
+			onrotate = true;
+
+			for(int i=0;i < 900;i++){
+				gameObject.transform.Rotate(0f,0.1f*Time.deltaTime,0f);
+			}
+
+			onrotate = false;
+		} 
+		*/
+
+		onrotate = true;
+	}
+}
---
+++ comportamiento_peatones/demo/emergencia/Assets/democine.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class democine : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		this.GetComponent<Animator>().Play("CameraRoll",-1,0.0f);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
