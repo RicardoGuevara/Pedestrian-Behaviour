685b9beddcfe38f38b924dc3e5b8390b7e288cc4
vrlls <seviloria@uninorte.edu.co>
2019-04-14 09:55:09 -0500

    nuevo estado

---
+++ evacuaciones/EvacuacionesRV/Assets/Destroyer21.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Destroyer21 : MonoBehaviour
+{
+
+    // Use this for initialization
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        if (this.transform.position.z >= -23)
+        {
+            Rotador();
+        }
+      //  if (this.transform.position.z >= -16.27) {
+        //    Destruction();
+        //}
+
+    }
+
+    void Destruction()
+    {
+        Destroy(this.gameObject);
+    }
+    void Rotador() {
+        this.transform.Rotate(0, 23, 0);
+    }
+}
